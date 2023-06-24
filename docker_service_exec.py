a4243a34def376e096d9843f183aa80f8f031907
Martin Braun <braun@neuroforge.de>
2023-06-25 00:01:22 +0200

    wip

--- docker_service_exec.py
+++ docker_service_exec.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 import random
 import string
 import subprocess
@@ -5,20 +6,21 @@ import os
 import docker
 import time
 import sys
+import click
+from typing import List
+import shlex
 
-if sys.argv[1] == 'docker-cli-plugin-metadata':
-    print(f"""
+if len(sys.argv) == 2:
+    if sys.argv[1] == 'docker-cli-plugin-metadata':
+        print("""
 {
-     "SchemaVersion": "0.1.0",
-     "Vendor": "Martin Braun",
-     "Version": "0.0.1",
-     "ShortDescription": "Docker Swarm Exec"
+    "SchemaVersion": "0.1.0",
+    "Vendor": "Martin Braun",
+    "Version": "0.0.1",
+    "ShortDescription": "Docker Swarm Exec"
 }         
-""")
-    exit(0)
-
-target_service = 'vibrant_bell'
-cmd = '/bin/bash'
+    """)
+        exit(0)
 
 def get_random_string(length):
     return ''.join(random.choice(string.ascii_letters) for i in range(length))
@@ -27,13 +29,14 @@ random_str = get_random_string(32)
 
 stack_name = f"docker_swarm_proxy_{random_str}"
 network_name = stack_name
-service_name = "srv"
+proxy_service_name = "srv"
+proxy_shell_container_name = f"proxy_shell_{random_str}"
 
 TEMPLATE = f"""
 version: "3.8"
 
 services:
-  {service_name}:
+  {proxy_service_name}:
     image: tecnativa/docker-socket-proxy
     volumes:
         - /var/run/docker.sock:/var/run/docker.sock
@@ -82,110 +85,166 @@ if os.path.isfile("/bin/docker"):
 elif os.path.isfile("/usr/bin/docker"):
     docker_binary = "/usr/bin/docker"
 
-def get_running_tasks(service):
-    return [
-        task
-        for task in service.tasks()
-        if  "Spec" in task 
-        and "DesiredState" in task
-        and task["DesiredState"] == "running"
-        and "Status" in task
-        and "State" in task["Status"]
-        and task["Status"]["State"] == "running"
-    ]
+@click.group()
+def cli() -> None:
+    pass
 
+@click.group()
+def service() -> None:
+    """
+    Docker Swarm Service Utilities
+    """
+    pass
 
-def get_service(name):
-    services = from_env.services.list(filters={"name": name})
-    if len(services) != 1:
-        raise AssertionError(f'did not find exactly one service with name {name}')
-    return services[0]
-
-needs_cleanup = False
-try:
-    from_env = docker.from_env()
-
-    service = get_service(target_service)
-    
-    running_tasks = get_running_tasks(service)
-    if len(running_tasks) == 0:
-        raise AssertionError(f"didn't find running task for service {target_service}")
-    
-    running_task = running_tasks[0]
-    node_id_running_task = running_task["NodeID"]
-    container_id = running_task["Status"]["ContainerStatus"]["ContainerID"]
-
-    # TODO: dont deploy the service to all nodes, but instead only
-    # to the one we care about that is running the task
-    needs_cleanup = True
-    subprocess.run(
-        [docker_binary, "stack", "deploy", "-c", "-", stack_name],
-        env={
-            **os.environ,
-        },
-        cwd=os.getcwd(),
-        input=TEMPLATE.encode('utf-8'),
-        check=True
-    )
-
-    while True:
-        # wait for proxy service to be there
-        service = get_service(f'{stack_name}_{service_name}')
-        all_tasks = service.tasks()
-        desired_running = [
+@service.command('exec')
+@click.option('-i', '--interactive', is_flag=True, show_default=True, default=False, help='Keep STDIN open even if not attached')
+@click.option('-t', '--tty', is_flag=True, show_default=True, default=False, help='Allocate a pseudo-TTY')
+@click.option('-u', '--user', help='Username or UID (format: "<name|uid>[:<group|gid>]")')
+@click.argument('service')
+@click.argument('command')
+@click.argument('arg', nargs=-1)
+def service_exec(
+    interactive: bool,
+    tty: bool,
+    user: string,
+    service: string,
+    command: string,
+    arg: List[str]
+):
+    def get_running_tasks(service):
+        return [
             task
-            for task in all_tasks
+            for task in service.tasks()
             if  "Spec" in task 
             and "DesiredState" in task
             and task["DesiredState"] == "running"
-        ]
-        actually_running = [
-            task
-            for task in desired_running
-            if "Status" in task
+            and "Status" in task
             and "State" in task["Status"]
             and task["Status"]["State"] == "running"
         ]
-        if len(desired_running) != len(actually_running):
-            time.sleep(1)
-        else:
-            break
 
-    subprocess.run(
-        [docker_binary, "run", "--network", network_name, "--rm", "-it", "--entrypoint", "/bin/sh", "ghcr.io/neuroforgede/docker-swarm-proxy/docker:master", "-c", f"""
-cat > script.py <<EOF
-import docker
-import dns.resolver
-import os
-import sys
-import subprocess
 
-answer = dns.resolver.resolve('tasks.{service_name}', 'A')
-for rdata in answer:    
-    client = docker.DockerClient(base_url=f'tcp://{'{'}rdata.address{'}'}:2375')
-    info = client.info()
+    def get_service(name):
+        services = from_env.services.list(filters={"name": name})
+        if len(services) != 1:
+            raise AssertionError(f'did not find exactly one service with name {name}')
+        return services[0]
 
-    node_id = info["Swarm"]["NodeID"]
-    if node_id == "{node_id_running_task}":
-        os.execvpe('/usr/local/bin/docker', ['/usr/local/bin/docker', 'exec', '-it', '{container_id}', '{cmd}'], env={'{'}
-            'DOCKER_HOST': 'tcp://' + str(rdata.address) + ':2375'
-        {'}'})
-EOF
-exec python3 script.py
-        """],
-        env={
-            **os.environ,
-        },
-        cwd=os.getcwd(),
-        check=True
-    )
-finally:
-    if needs_cleanup:
-        subprocess.run(
-            [docker_binary, "stack", "rm", stack_name],
-            env={
-                **os.environ,
-            },
-            cwd=os.getcwd(),
-            check=True
-        )
+    needs_cleanup = False
+    try:
+      from_env = docker.from_env()
+
+      service = get_service(service)
+      
+      running_tasks = get_running_tasks(service)
+      if len(running_tasks) == 0:
+          raise AssertionError(f"didn't find running task for service {service}")
+      
+      running_task = running_tasks[0]
+      node_id_running_task = running_task["NodeID"]
+      container_id = running_task["Status"]["ContainerStatus"]["ContainerID"]
+
+      # TODO: dont deploy the service to all nodes, but instead only
+      # to the one we care about that is running the task
+      needs_cleanup = True
+      subprocess.run(
+          [docker_binary, "stack", "deploy", "-c", "-", stack_name],
+          env={
+              **os.environ,
+          },
+          cwd=os.getcwd(),
+          input=TEMPLATE.encode('utf-8'),
+          check=True
+      )
+
+      while True:
+          # wait for proxy service to be there
+          service = get_service(f'{stack_name}_{proxy_service_name}')
+          all_tasks = service.tasks()
+          desired_running = [
+              task
+              for task in all_tasks
+              if  "Spec" in task 
+              and "DesiredState" in task
+              and task["DesiredState"] == "running"
+          ]
+          actually_running = [
+              task
+              for task in desired_running
+              if "Status" in task
+              and "State" in task["Status"]
+              and task["Status"]["State"] == "running"
+          ]
+          if len(desired_running) != len(actually_running):
+              time.sleep(1)
+          else:
+              break
+          
+      interactive_str = '-i' if interactive else ''
+      tty_str = '-t' if tty else ''
+      user_str = user or ''
+
+      docker_flags = [elem for elem in [interactive_str, tty_str] if elem != '']
+
+      subprocess.run(
+          [
+            docker_binary, 
+            "--env", f"PROXY_SERVICE_NAME={proxy_service_name}",
+            "--env", f"CONTAINER_ID={container_id}",
+            "--env", f"USER_FLAG={user_str}",
+            "--env", f"IS_TTY={tty_str}",
+            "--env", f"IS_INTERACTIVE={interactive_str}",
+            "run", 
+            "--name", proxy_shell_container_name,
+            "--network", network_name,
+            "--rm",
+            *docker_flags,
+            "ghcr.io/neuroforgede/docker-swarm-proxy/docker:master",
+            *arg
+          ],
+          env={
+              **os.environ,
+          },
+          cwd=os.getcwd(),
+          check=True
+      )
+    finally:
+      if needs_cleanup:
+          # hack, ensure that the proxy container is dead and removed
+          subprocess.run(
+              [docker_binary, "kill", proxy_shell_container_name],
+              env={
+                  **os.environ,
+              },
+              cwd=os.getcwd(),
+              stdout=None,
+              stderr=None,
+              check=False,
+              capture_output=True
+          )
+          # hack, ensure that the proxy container is dead and removed
+          subprocess.run(
+              [docker_binary, "rm", proxy_shell_container_name],
+              env={
+                  **os.environ,
+              },
+              cwd=os.getcwd(),
+              stdout=None,
+              stderr=None,
+              check=False,
+              capture_output=True
+          )
+
+          # do the proper cleanup of the stack
+          subprocess.run(
+              [docker_binary, "stack", "rm", stack_name],
+              env={
+                  **os.environ,
+              },
+              cwd=os.getcwd(),
+              check=True
+          )
+
+
+cli.add_command(service)
+cli()
---
+++ service-exec/service_exec.py
@@ -0,0 +1,35 @@
+import docker
+import dns.resolver
+import os
+import sys
+
+PROXY_SERVICE_NAME = os.environ['PROXY_SERVICE_NAME']
+CONTAINER_ID = os.environ['CONTAINER_ID']
+
+USER_FLAG = os.getenv('USER_FLAG', '')
+IS_TTY = os.getenv('IS_TTY', '')
+IS_INTERACTIVE = os.getenv('IS_INTERACTIVE')
+
+FLAGS = []
+
+if USER_FLAG != '':
+    FLAGS.append('-u')
+    FLAGS.append(USER_FLAG)
+
+if IS_TTY != '':
+    FLAGS.append('-t')
+
+if IS_INTERACTIVE != '':
+    FLAGS.append('-i')
+
+
+answer = dns.resolver.resolve(f'tasks.{PROXY_SERVICE_NAME}', 'A')
+for rdata in answer:    
+    client = docker.DockerClient(base_url=f'tcp://{rdata.address}:2375')
+    info = client.info()
+
+    node_id = info["Swarm"]["NodeID"]
+    if node_id == "{node_id_running_task}":
+        os.execvpe('/usr/local/bin/docker', ['/usr/local/bin/docker', 'exec', *FLAGS, CONTAINER_ID, sys.argv[1:]], env={
+            'DOCKER_HOST': 'tcp://' + str(rdata.address) + ':2375'
+        })
